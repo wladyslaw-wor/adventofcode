db401d3e23a100a34852f6bcd850900ad83ff511
vo_vorontsov <vo_vorontsov@betboom.org>
2023-12-04 03:49:15 +0300

    d01 part 2

---
+++ 2023/d01/part_02.py
@@ -0,0 +1,22 @@
+import re
+
+with open("input.txt", "r") as file:
+    data = file.read().strip()
+
+def calibration(data):
+    ls = data.split("\n")
+    ns = [re.findall("\d", x) for x in ls]
+    return sum(int(n[0] + n[-1]) for n in ns)
+
+data = (
+    data.replace("one", "one1one")
+    .replace("two", "two2two")
+    .replace("three", "three3three")
+    .replace("four", "four4four")
+    .replace("five", "five5five")
+    .replace("six", "six6six")
+    .replace("seven", "seven7seven")
+    .replace("eight", "eight8eight")
+    .replace("nine", "nine9nine")
+)
+print(calibration(data))
